e9422c3aa17e8121122d656cce8a138d2e2ad690
Dawid Rozak <dawid.rozak.dev@gmail.com>
2021-04-27 17:54:31 +0200

    Repair link in Card.js to render image and small edit .css (posters)

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -12,7 +12,7 @@ const Card = () => {
 
     const ShowMovies = myLikedMovies.map(item => (
         <div key={item.title} className="poster-box">
-            <Link className="poster-child" to={`/catalogue/${item.lp}`}>
+            <Link className="poster-child" to={process.env.PUBLIC_URL + `/catalogue/${item.lp}`}>
                 <img id="poster" src={`/images/${item.lp}/poster.jpg`} alt="poster"/>    
             </Link> 
             <button id="remove-liked" onClick={removeLikedMovie.bind(this, item.lp)} >Remove</button>
